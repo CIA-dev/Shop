1de7aa2ba1f195fe546b82b9259cc5bde4b8c0d7
YasinDehfuli <Ydehfuli@Gmail.Com>
2022-11-13 20:25:35 +0330

    add BODY PART2 and Owl carousel + style for owl carousel in y-style.css and some changes to old carousel style and new one !

--- assets/js/y-script.js
+++ assets/js/y-script.js
@@ -17,3 +17,22 @@ $(function () {
         }
     });
 })
+$(function () {
+    $('#body-part2 .owl-carousel').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+})
