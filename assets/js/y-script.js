29b124f6dd15d67de654ada95f995237b8c781c4
YasinDehfuli <Ydehfuli@Gmail.Com>
2022-11-14 17:31:46 +0330

    add BODY PART3 labtop and computer to index.html

--- assets/js/y-script.js
+++ assets/js/y-script.js
@@ -36,3 +36,22 @@ $(function () {
         }
     });
 })
+$(function () {
+    $('#body-part3 .owl-carousel').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+})
